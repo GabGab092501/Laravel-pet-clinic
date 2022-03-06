fc1b1ec35c7579b6110846cb33600869cde9cca5
IamGab <gabrielarafol.mendoza@tup.edu.ph>
2022-03-06 20:37:45 +0800

    Add search and result function

--- app/Http/Controllers/animalController.php
+++ app/Http/Controllers/animalController.php
@@ -12,6 +12,63 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Requests\animalRequest;
 class animalController extends Controller
 {
+    public function search()
+    {
+        $animals = Animal::leftJoin(
+            "animal_disease_injury",
+            "animals.id",
+            "=",
+            "animal_disease_injury.animals_id"
+        )
+            ->leftJoin(
+            "disease_injuries",
+            "disease_injuries.id",
+            "=",
+            "animal_disease_injury.disease_injury_id"
+            )
+            ->select("disease_injuries.classify",
+            "animals.id",
+            "animals.animal_name",
+            "animals.age",
+            "animals.gender",
+            "animals.type",
+            "animals.images")
+            ->orderBy("animals.id", "ASC")
+            ->get();
+         return view('animals.search',[
+            'animals' => $animals
+        ]);
+    }
+
+    public function result()
+    {
+        $result = $_GET['result'];
+        $animals = Animal::leftJoin(
+            "animal_disease_injury",
+            "animals.id",
+            "=",
+            "animal_disease_injury.animals_id"
+        )
+            ->leftJoin(
+            "disease_injuries",
+            "disease_injuries.id",
+            "=",
+            "animal_disease_injury.disease_injury_id"
+            )
+            ->select("animals.id",
+            "animals.animal_name",
+            "animals.age",
+            "animals.gender",
+            "animals.type",
+            "animals.images")
+
+        ->where('animals.animal_name', 'LIKE', '%'.$result.'%')
+        ->get();
+        return view('animals.result',[
+            'animals' => $animals
+        ]);
+    }
+
     /**
      * Display a listing of the resource.
      *
