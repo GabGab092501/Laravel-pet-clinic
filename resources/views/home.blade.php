8fb0a3ed302c62c14bf06f889c1dac6868785892
IamGab <gabrielarafol.mendoza@tup.edu.ph>
2022-03-16 22:18:27 +0800

    Add Auth session to call it

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<h1 class="text-center text-5xl pb-8 pt-6 text-blue-600">Welcome To ACME Pet Clinic,  {{ Auth::user()->full_name }}</h1>
+<hr>
+<div class="py-3">
+  <table class="table-auto">
+    <tr class="text-center">
+      <th class="w-screen text-4xl">Id</th>
+      <th class="w-screen text-4xl">Position</th>
+      <th class="w-screen text-4xl">Email</th>
+    </tr>
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+    <tr>
+      <td class=" text-center text-4xl text-white">
+        {{ Auth::user()->id }}
+      </td>
+      <td class=" text-center text-4xl text-white">
+        {{ Auth::user()->role }}
+      </td>
+      <td class=" text-center text-4xl text-white">
+        {{ Auth::user()->email }}
+      </td>
+    </tr>
+  </table>
 </div>
+</div>
+<hr>
+<h1 class="text-center text-5xl pt-20 px-4 text-green-600">Our mission is to provide the highest quality animal care to
+  your pet and improve his or her quality of life through the preservation, enhancement, and restoration of your pets
+  health.</h1>
+
 @endsection
