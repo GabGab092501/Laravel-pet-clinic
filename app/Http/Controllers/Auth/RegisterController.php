66b60e42f46a4d0bf6c3ca73be92949273723b7b
IamGab <gabrielarafol.mendoza@tup.edu.ph>
2022-03-16 22:15:50 +0800

    Change user to personnel and adjust some details

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use App\Models\User;
+use App\Models\Personnel;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -50,9 +50,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'full_name' => ['required', 'string', 'min:5'],
+            'email' => ['required', 'string', 'email', 'unique:personnels'],
+            'password' => ['required', 'min:5', 'confirmed'],
         ]);
     }
 
@@ -60,14 +60,15 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\Models\User
+     * @return \App\Models\Personnel
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
+        return Personnel::create([
+            'full_name' => $data['full_name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'role' => $data['role'],
         ]);
     }
 }
