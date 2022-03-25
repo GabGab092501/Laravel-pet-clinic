114c6bd5aa625f5a4bcff448638f17610478393f
IamGab <gabrielarafol.mendoza@tup.edu.ph>
2022-03-25 13:03:52 +0800

    Use sweet alert and login request

--- app/Http/Controllers/personnelController.php
+++ app/Http/Controllers/personnelController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\personnelRequest;
 use App\Http\Requests\personnelUpdateController;
+use App\Http\Requests\loginRequest;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 use App\Models\Personnel;
-
+use Illuminate\Support\Facades\Auth;
+use RealRashid\SweetAlert\Facades\Alert;
 class personnelController extends Controller
 {
     // public function login()
