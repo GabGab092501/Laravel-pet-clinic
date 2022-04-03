d5c1b05a12e92c125f70163b281349e3f67ef0dc
Dave <daveavrosia@gmail.com>
2022-04-04 04:06:45 +0800

    Add ServiceSeeder

---
+++ database/seeders/ServiceSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ServiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Service::factory()->count(1000)->create(); 
+    }
+}
