680941a0732379b2c957b9ff5cdefa14a933492e
IamGab <gabrielarafol.mendoza@tup.edu.ph>
2022-04-04 21:11:44 +0800

    Update contact migration

--- database/migrations/2022_03_07_131825_create_contacts_table.php
+++ database/migrations/2022_03_07_131825_create_contacts_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -18,8 +19,14 @@ return new class extends Migration {
             $table->string(column: "email");
             $table->string(column: "phone_number");
             $table->string(column: "review");
+            $table->integer(column: "service_id")->unsigned();
             $table->timestamps();
             $table->softDeletes();
+            $table
+                ->foreign("service_id")
+                ->references("id")
+                ->on("services")
+                ->onDelete("cascade");
         });
     }
 
