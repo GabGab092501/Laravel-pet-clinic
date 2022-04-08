49de5fc0d3f97e8bd17c01e986e20135db8f0cc6
IamGab <gabrielarafol.mendoza@tup.edu.ph>
2022-04-09 03:02:09 +0800

    Add animal data and section to seperate the data

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -13,24 +13,55 @@
 </div>
 @endif
 
-<body class="grid grid-flow-col gap-3 p-12 w-full"
-    style="background-image:linear-gradient(rgba(212, 212, 212, 0.1),rgba(212,212,212,0.1)), url(https://wallpapercave.com/wp/B1sODrM.jpg); background-size:cover;">
-    @foreach ($services->chunk(1) as $serviceChunk)
-    <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-        @foreach ($serviceChunk as $service)
-        <img src="{{ asset('uploads/services/'.$service->images)}}" alt="I am A Pic" width="400">
-        <div class="p-3">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight">{{ $service->service_name }}
-            </h5>
-            <p class="mb-2 text-lg font-bold">{{ $service->cost }}</p>
-            <div class="grid grid-flow-col gap-1">
-                <a href=" {{ route('transaction.addToCart', ['id'=>$service->id]) }} " class="btn btn-primary"
-                    role="button"><i class="fas fa-cart-plus"></i> Add to Cart</a>
+<body style="background-image:linear-gradient(rgba(212, 212, 212, 0.1),rgba(212,212,212,0.1)),
+    url(https://wallpapercave.com/wp/B1sODrM.jpg); background-size:cover;">
+    <div class="pt-8 pb-4 px-8">
+        <a href="{{url()->previous()}}" class="p-3 border-none italic text-white bg-black text-lg">
+            Go Back &rarr;
+        </a>
+    </div>
+    <h1 class="text-center text-3xl text-white">CHOOSE YOUR PET</h1>
+    <section class="flex flex-wrap justify-center gap-3 p-12 w-full">
+        @foreach ($animals->chunk(1) as $serviceChunk)
+        <div
+            class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+            @foreach ($serviceChunk as $animal)
+            <img src="{{ asset('uploads/animals/'.$animal->images)}}" alt="I am A Pic" width="400">
+            <div class="p-3">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight">{{ $animal->animal_name }}
+                </h5>
+                <p class="mb-2 text-lg font-bold">{{ $animal->type }}</p>
+                <div class="grid grid-flow-col gap-1">
+                    <a href=" {{ route('transaction.addAnimal', ['id'=>$animal->id]) }} " class="btn btn-primary"
+                        role="button"><i class="fas fa-cart-plus"></i> Add to Animal</a>
 
-                <a href="#" class="btn btn-success" role="button">More Info</a>
+                    <a href="{{ route('personnels.dashboard') }}" class="btn btn-success" role="button">Back</a>
+                </div>
             </div>
         </div>
-    </div>
-    @endforeach
-    @endforeach
+        @endforeach
+        @endforeach
+    </section>
+    <h1 class="text-center text-3xl text-white pt-3">CHOOSE YOUR SERVICE</h1>
+    <section class="flex flex-wrap gap-3 justify-center p-12 w-full">
+        @foreach ($services->chunk(1) as $serviceChunk)
+        <div
+            class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+            @foreach ($serviceChunk as $service)
+            <img src="{{ asset('uploads/services/'.$service->images)}}" alt="I am A Pic" width="400">
+            <div class="p-3">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight">{{ $service->service_name }}
+                </h5>
+                <p class="mb-2 text-lg font-bold">{{ $service->cost }}</p>
+                <div class="grid grid-flow-col gap-1">
+                    <a href=" {{ route('transaction.addToCart', ['id'=>$service->id]) }} " class="btn btn-primary"
+                        role="button"><i class="fas fa-cart-plus"></i> Add to Cart</a>
+
+                    <a href="{{ route('personnels.dashboard') }}" class="btn btn-success" role="button">Back</a>
+                </div>
+            </div>
+        </div>
+        @endforeach
+        @endforeach
+    </section>
 </body>
