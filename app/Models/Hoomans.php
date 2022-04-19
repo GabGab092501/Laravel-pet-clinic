bbb02943b30f5e78a48e7361c98eb70e27dd97ca
Heinrich Fabros <johnheinrich.fabros@tup.edu.ph>
2022-04-19 19:22:14 +0800

    Update Hoomans models

---
+++ app/Models/Hoomans.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+class Hoomans extends Authenticatable
+{
+    public const RULES = [
+        "name" => ["required", "min:3"],
+        "email" => ["required", "string", "email", "unique:hoomans"],
+        "password" => ["required", "min:5", "confirmed"],
+        "role" => ["required", "alpha", ],
+    ];
+
+    protected $dates = ["deleted_at"];
+
+    protected $table = "hoomans";
+
+    protected $primaryKey = "id";
+
+    protected $guarded = ["id"];
+
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ["name", "email", "password", "role"];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = ["password"];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        "email_verified_at" => "datetime",
+    ];
+}
