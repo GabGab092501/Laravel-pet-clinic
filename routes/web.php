cae5091d5efddba6c0153328b40b56a984cd9ded
Heinrich Fabros <johnheinrich.fabros@tup.edu.ph>
2022-04-19 19:48:25 +0800

    Update php web routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\animalController;
+use App\Http\Controllers\petsController;
 use App\Http\Controllers\customerController;
-use App\Http\Controllers\personnelController;
+use App\Http\Controllers\hoomansController;
 use App\Http\Controllers\serviceController;
 use App\Http\Controllers\contactController;
 use App\Http\Controllers\UploadController;
@@ -34,18 +34,17 @@ Route::get("/contact/forceDelete/{id}", [
 Route::get("/review", [contactController::class, "review"])->name("review");
 Route::post("/send", [contactController::class, "send"])->name("send");
 
-Route::resource("/animals", "animalController")->middleware("auth");
-Route::get("/animals/restore/{id}", [
-    "uses" => "animalController@restore",
-    "as" => "animals.restore",
+Route::resource("/pets", "petsController")->middleware("auth");
+Route::get("/pets/restore/{id}", [
+    "uses" => "petsController@restore",
+    "as" => "pets.restore",
 ]);
-Route::get("/animals/forceDelete/{id}", [
-    "uses" => "animalController@forceDelete",
-    "as" => "animals.forceDelete",
+Route::get("/pets/forceDelete/{id}", [
+    "uses" => "petsController@forceDelete",
+    "as" => "pets.forceDelete",
 ]);
 
 Route::resource("/customer", "customerController")->middleware("auth");
-//Route::resource("/customer", customerController::class);
 Route::get("/customer/restore/{id}", [
     "uses" => "customerController@restore",
     "as" => "customer.restore",
@@ -55,15 +54,14 @@ Route::get("/customer/forceDelete/{id}", [
     "as" => "customer.forceDelete",
 ]);
 
-Route::resource("/personnel", "personnelController")->middleware("auth");
-//Route::resource("/personnel", personnelController::class);
-Route::get("/personnel/restore/{id}", [
-    "uses" => "personnelController@restore",
-    "as" => "personnel.restore",
+Route::resource("/hoomans", "hoomansController")->middleware("auth");
+Route::get("/hoomans/restore/{id}", [
+    "uses" => "hoomansController@restore",
+    "as" => "hoomans.restore",
 ]);
-Route::get("/personnel/forceDelete/{id}", [
-    "uses" => "personnelController@forceDelete",
-    "as" => "personnel.forceDelete",
+Route::get("/hoomans/forceDelete/{id}", [
+    "uses" => "hoomansController@forceDelete",
+    "as" => "hoomans.forceDelete",
 ]);
 
 Route::resource("/service", serviceController::class)->middleware("auth");
@@ -85,10 +83,7 @@ Route::get("/consultation/forceDelete/{id}", [
     "uses" => "consultationController@forceDelete",
     "as" => "consultation.forceDelete",
 ]);
-//Route::get('/search', 'App\Http\Controllers\consultationController@search')->name("search")->middleware("auth");
-//Route::get("/search", [consultationController::class, "search"])->name("search");
 Route::get('/results', 'App\Http\Controllers\consultationController@results')->name("results")->middleware("auth");
-//Route::get("/result", [consultationController::class, "result"])->name("result");
 Route::get('/result', 'App\Http\Controllers\customerController@result')->name("result")->middleware("auth");
 
 Route::resource("/transaction", transactionController::class)->middleware("auth");
@@ -96,61 +91,48 @@ Route::resource("/transaction", transactionController::class)->middleware("auth"
 Route::get("/", function () {
     return view("welcome");
 });
+Route::get('/homewelcome', function () {
+    return view('homewelcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
 
 Route::get("signup", [
-    "uses" => "personnelController@getSignup",
-    "as" => "personnel.signup",
+    "uses" => "hoomansController@getSignup",
+    "as" => "hoomans.signup",
 ])->middleware("guest");
 
 Route::post("signup", [
-    "uses" => "personnelController@postSignup",
-    "as" => "personnel.signup",
+    "uses" => "hoomansController@postSignup",
+    "as" => "hoomans.signup",
 ])->middleware("guest");
 
 Route::get("dashboard", [
-    "uses" => "personnelController@Dashboard",
-    "as" => "personnels.dashboard",
+    "uses" => "hoomansController@Dashboard",
+    "as" => "hoomans.dashboard",
 ])->middleware("auth");
 
 Route::post("logout", [
-    "uses" => "personnelController@getLogout",
-    "as" => "personnel.logout",
+    "uses" => "hoomansController@getLogout",
+    "as" => "hoomans.logout",
 ]);
 
 Route::get("logout", [
-    "uses" => "personnelController@getLogout",
-    "as" => "personnel.logout",
+    "uses" => "hoomansController@getLogout",
+    "as" => "hoomans.logout",
 ]);
 
 Route::post("signin", [
-    "uses" => "personnelController@postSignin",
-    "as" => "personnel.signin",
+    "uses" => "hoomansController@postSignin",
+    "as" => "hoomans.signin",
 ])->middleware("guest");
 
 Route::get("signin", [
-    "uses" => "personnelController@getSignin",
-    "as" => "personnel.signin",
+    "uses" => "hoomansController@getSignin",
+    "as" => "hoomans.signin",
 ])->middleware("guest");
-
-Route::post("email", [
-    "uses" => "personnelController@Email",
-    "as" => "personnel.email",
-]);
-
-Route::get("email", [
-    "uses" => "personnelController@Email",
-    "as" => "personnel.email",
-]);
-
-Route::post("reset", [
-    "uses" => "personnelController@Reset",
-    "as" => "personnel.reset",
-]);
-
-Route::get("reset", [
-    "uses" => "personnelController@Reset",
-    "as" => "personnel.reset",
-]);
 
 Route::get('shopping-cart', [
     'uses' => 'App\Http\Controllers\transactionController@getCart',
