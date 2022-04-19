b04887bad9c952f5d5c7482d4b4e5d59e457fb36
Heinrich Fabros <johnheinrich.fabros@tup.edu.ph>
2022-04-19 19:42:47 +0800

    Update result blade consultation

--- resources/views/consultations/result.blade.php
+++ resources/views/consultations/result.blade.php
@@ -1,21 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('home')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>Search Pets</title>
-</head>
+@section('contents')
 
-<body
-    style="background-image:linear-gradient(rgba(212, 212, 212, 0.1),rgba(212,212,212,0.1)), url(https://wallpapercave.com/wp/B1sODrM.jpg); background-size:cover;">
+<body>
+   
     <div class="py-3">
-        <h1 class="text-center text-blue-600 text-3xl font-bold py-6">CONSULTATION HISTORY</h1>
-        <hr>
+        <h1 class="text-center text-black-600 text-3xl font-bold py-3">History</h1>
+   
         <table class="table-auto">
-            <tr class="text-white text-center">
+            <tr class="text-black text-center">
                 <th class="w-screen text-4xl">Animal</th>
                 <th class="w-screen text-4xl">Date</th>
                 <th class="w-screen text-4xl">Disease or Injury</th>
@@ -36,17 +29,14 @@
                     {{ $consultation->full_name }}
                 </td>
                 @empty
-                <p class="text-center text-4xl py-8">The Animal You Search Is Not In The Database.</p>
+                <p class="text-center text-4xl py-8">Data is empty</p>
                 @endforelse
         </table>
     </div>
     </tr>
-    <hr>
-    <h1 class="text-center text-5xl py-8 text-red-600">Thank you for Choosing ACME Pet Clinic</h1>
     <div class="flex justify-end">
-        <a href="{{url()->previous()}}" class="bg-gray-800 text-white text-2xl font-bold p-2 mr-10 text-center"
+        <a href="{{url()->previous()}}" class="bg-gray-800 text-black text-2xl font-bold p-2 mr-10 text-center"
             role="button">Go Back &rarr;</a>
     </div>
 </body>
-
-</html>
+@endsection
