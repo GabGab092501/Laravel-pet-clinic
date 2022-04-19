5e4cba12d68a754eb3233b1ee5bc12b49281aab3
Heinrich Fabros <johnheinrich.fabros@tup.edu.ph>
2022-04-19 19:22:28 +0800

    Update Pets models

---
+++ app/Models/Pets.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class Pets extends Model
+{
+    public const RULES = [
+        "pets_name" => ["required", "alpha", "min:3"],
+        "age" => ["required", "numeric", "min:1", "max:20"],
+        "gender" => ["required", "alpha", "string", "min:3"],
+        "type" => ["required", "alpha", "min:3", "string"],
+        "images" => ["required", "image", "mimes:jpg,png,jpeg,gif", "max:5048"],
+    ];
+
+    use HasFactory;
+
+    use SoftDeletes;
+
+    protected $dates = ["deleted_at"];
+
+    protected $table = "pets";
+
+    protected $primaryKey = "id";
+
+    protected $guarded = ["id"];
+
+}
